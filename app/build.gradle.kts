9155969735897ac16796a58026c8dd6ee4c5e384
PixelGamer4k <fergpompom222@gmail.com>
2025-11-23 19:54:20 +0530

    fix: Add material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     
     // Permissions
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
